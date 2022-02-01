f07611e9b7242917abbede2d1d1aa82ea346ac47
Chrissie Sparling <gigglewrks@aol.com>
2022-02-01 13:35:31 -0800

    completed project for terminal command output

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO movies (movie_name)
+VALUES ("Star Wars"),
+       ("Moana"),
+       ("Into The Woods"),
+       ("The Long Kiss Goodnight"),
+       ("The Wizard of Oz");
+
+INSERT INTO reviews (movie_id, review)
+VALUES (1, "an epic battle between good and evil set in a galaxy far far away!"),
+       (2, "a hawaiian girl sets out on an adventure to put the heart of Tafeetti back in its place"),
+       (3, "Combination of stories like little red riding hood, cinderella, sleeping beauty, jack in the bean stalk- all sung in this epic opera style movie"),
+       (4, "my favorite christmas movie"),
+       (5, "The cowardly lion just needs a hug."),
+       (1, "May the Force be with you"),
+       (3, "don't stray from the path...little red...don't do it!");
